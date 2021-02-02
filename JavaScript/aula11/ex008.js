77b4d1c4457b2899193b1bb04859c9081b2320b8
GenesisHenriques <54963919+GenesisHenriques@users.noreply.github.com>
2021-02-02 17:49:13 -0300

    Add files via upload

---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log (`A velocidade do seu carro é ${vel}`)
+if (vel > 60){
+    console.log(`Sua velocidade está acima da permitida, MULTADO!`)
+}
+console.log(`Dirija com cuidado!`)
