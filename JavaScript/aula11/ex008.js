7004f0193a4dfefbe01d379fcf770200a2250841
Genesis-Henriques <GenesisHenriques@gmail.com>
2021-02-02 17:58:07 -0300

    Aulas feitas até aqui

--- JavaScript/aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 50.5
-console.log (`A velocidade do seu carro é ${vel}`)
-if (vel > 60){
-    console.log(`Sua velocidade está acima da permitida, MULTADO!`)
-}
-console.log(`Dirija com cuidado!`)
--- JavaScript/aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var pais = 'EUA'
-console.log(`Viivendo em ${pais}`)
-if (pais == 'BRASIL') {
-    console.log('você é Brasileira')
-} else {
-    console.log('você é Estrangeiro')
-}
